1ed4a86e3f1e27789e92e2024e1e1435d726e731
ValeriiRuchko <valerii.ruchko@gmail.com>
2024-10-09 17:29:19 +0200

    Add Turso for DB tests. Adjust local-dev config. Change .gitignore.

--- reverse-proxy-experiment/go.mod
+++ reverse-proxy-experiment/go.mod
@@ -6,11 +6,14 @@ require (
 	github.com/aws/aws-lambda-go v1.47.0
 	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2
 	github.com/gin-gonic/gin v1.9.1
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240902231107-85af5b9d094d
 )
 
 require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/coder/websocket v1.8.12 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -29,8 +32,9 @@ require (
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/net v0.24.0 // indirect
-	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- reverse-proxy-experiment/proxy/main.go
+++ reverse-proxy-experiment/proxy/main.go
@@ -2,22 +2,85 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/gin"
 	"github.com/gin-gonic/gin"
+
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
+
+type User struct {
+	ID   int
+	Name string
+}
+
+func queryUsers(db *sql.DB) ([]User, error) {
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute query: %v\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	var users []User
+
+	for rows.Next() {
+		var user User
+
+		if err := rows.Scan(&user.ID, &user.Name); err != nil {
+			fmt.Println("Error scanning row:", err)
+			return nil, err
+		}
+
+		users = append(users, user)
+		fmt.Println(user.ID, user.Name)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error during rows iteration:", err)
+	}
+	return users, nil
+}
 
 var ginLambda *ginadapter.GinLambdaV2
 
 func init() {
+	url := os.Getenv("TursoDatabaseURL") + "?authToken=" + os.Getenv("TursoAuthToken")
+
+	db, err := sql.Open("libsql", url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open db %s: %s", url, err)
+		os.Exit(1)
+	}
+	// defer db.Close()
+
+	// db.Exec("CREATE TABLE test (id SERIAL, user TEXT NOT NULL, name TEXT NOT NULL);")
+	// db.Exec("INSERT INTO test (user, name) VALUES ('Test user 1', 'Name is not important');")
+	//
+	// fmt.Println("Db started successfully")
+
 	log.Printf("Gin cold start")
+
 	r := gin.Default()
+
+	// TODO: uncomment line below for release
+	// gin.SetMode(gin.ReleaseMode);
+
 	r.GET("/hello", func(c *gin.Context) {
+		all_users, err := queryUsers(db)
+		if err != nil {
+			fmt.Println("FUCK")
+		}
+
 		c.JSON(200, gin.H{
 			"message": "Yo lets try",
+			"users":   all_users,
 		})
 	})
 	r.GET("/hello/me", func(c *gin.Context) {
